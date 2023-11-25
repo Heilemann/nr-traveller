9839412fb19eca0a8d7880b361e3cb766f9a3efd
Michael Heilemann <michael.heilemann@me.com>
2023-11-24 20:58:20 -0500

    Updating

--- src/components/BaseComponents/Asset.tsx
+++ src/components/BaseComponents/Asset.tsx
@@ -31,13 +31,13 @@ const Asset: FC<AssetProps> = props => {
 
 	// should move this to a context
 	let parentOrigin = ''
-	// if (process.env.NODE_ENV === 'development') {
-	// const protocol = window.location.protocol;
-	// const host = window.location.hostname;
-	parentOrigin = `http://localhost:3000`
-	// } else {
-	// 	parentOrigin = 'https://newreal.ms'
-	// }
+	if (process.env.NODE_ENV === 'development') {
+		// const protocol = window.location.protocol;
+		// const host = window.location.hostname;
+		parentOrigin = `http://localhost:3000`
+	} else {
+		parentOrigin = 'https://newreal.ms'
+	}
 
 	const handleUpload = () => {
 		messageToApp({
