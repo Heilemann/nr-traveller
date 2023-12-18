0d18bc61750c10580215c5cdadb9dc38a120facc
Michael Heilemann <michael.heilemann@me.com>
2023-12-18 17:09:02 -0500

    Updating

--- src/components/BaseComponents/Form/Textarea.tsx
+++ src/components/BaseComponents/Form/Textarea.tsx
@@ -1,17 +1,22 @@
-import { forwardRef } from 'react'
+import { forwardRef, useContext } from 'react'
 import ReactTextareaAutosize from 'react-textarea-autosize'
 import { twMerge } from 'tailwind-merge'
+import context from '../context'
 
 interface ITextareaProps
 	extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
 const TextArea = forwardRef<HTMLTextAreaElement, ITextareaProps>(
 	({ className, style, ...rest }, ref) => {
+		const { state } = useContext(context)
+		const { editMode } = state
+
 		return (
 			<ReactTextareaAutosize
 				ref={ref}
 				className={twMerge(
-					`leading-16 mb-1 mt-1 block w-full rounded-md border-none bg-gray-800/50 p-2 text-white placeholder-gray-400 shadow-none placeholder:text-gray-500`,
+					`leading-16 mb-1 mt-1 block w-full rounded-md border-none bg-gray-800/50 p-2 text-white placeholder-gray-400 shadow-none transition-all duration-200 ease-in-out placeholder:text-gray-500`,
+					editMode === 'view' ? 'bg-transparent p-0' : '',
 					className,
 				)}
 				{...rest}
