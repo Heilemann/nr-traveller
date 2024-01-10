3e406c2a3d1232c257409442ad9f8495cd09df5c
Michael Heilemann <michael.heilemann@me.com>
2024-01-09 21:05:07 -0500

    Updating

--- src/components/BaseComponents/Container.tsx
+++ src/components/BaseComponents/Container.tsx
@@ -27,7 +27,7 @@ export default function Container() {
 
 		const subscription = watch(values => {
 			if (!values || !state.document) return
-			if (_.isEqual(values, state.document.values)) return
+			// if (_.isEqual(values, state.document.values)) return
 			if (resetInProgress.current) {
 				resetInProgress.current = false
 				return
